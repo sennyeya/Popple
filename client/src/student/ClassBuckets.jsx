03965e882ed50f36bf9fc211b485cbd6671ee558
asennyey <asennyey@email.arizona.edu>
2020-11-17 21:36:04 -0700

    Added in automatic class non-draggable. Updated boxes to include scroll-bars.

--- client/src/student/ClassBuckets.jsx
+++ client/src/student/ClassBuckets.jsx
@@ -4,7 +4,9 @@ import {LoadingIndicator} from '../shared/Loading';
 import {InfoModal} from './ClassModal';
 import {Collapse} from 'react-collapse';
 import {BiCaretDown, BiCaretRight, BiError} from 'react-icons/bi';
-import style from './ClassBuckets.module.css'
+import {BsGearFill, BsList, BsListNested} from 'react-icons/bs';
+import style from './ClassBuckets.module.css';
+import {Form, InputGroup} from 'react-bootstrap';
 
 const PRIMARY_BUCKET = "";
 const grid = 8;
@@ -16,6 +18,7 @@ export default function ClassBuckets({API, setSelected, openClassModal}){
     const [missingClasses, setMissingClasses] = React.useState([])
     const [isModalOpen, setModalOpen] = React.useState(false);
     const [modalMessage, setModalMessage] = React.useState("");
+    const [areAllOpen, setAllOpen] = React.useState(true);
     
     const reorder = (bucket, source, destination)=>{
         var array = [...nodes];
@@ -42,10 +45,6 @@ export default function ClassBuckets({API, setSelected, openClassModal}){
             }
         }
         filteredArr[sourceIndex].bucket = destination;
-        let dependents = [...missingClasses];
-        dependents = dependents.filter(e=>e.id!==filteredArr[sourceIndex].id);
-
-        setMissingClasses(dependents)
         setNodes(array)
 
         API.post(`/student/bucket/move`, {
@@ -64,13 +63,6 @@ export default function ClassBuckets({API, setSelected, openClassModal}){
                 dependents.filter(e=>e.bucket===mainBucket).length
             )
         }
-    }
-
-    const getModalMissingMessage = (currentNode, filteredArr) => {
-        var missingClasses = nodes.filter(node=>(currentNode.children.filter(e=>!filteredArr.some(f=>f.id===e))).some(child=>node.id===child))
-        setMissingClasses(missingClasses)
-        return `Class ${currentNode.label} is missing the following prerequisite(s): \n\t${missingClasses.map(e=>e.label).join(", ")}. 
-                    Those prerequisites have been highlighted in red in your Required Courses. For more information on prerequisites, look at the Degree Path diagram.`
     }
 
     const getDependents = (node, arr) =>{
@@ -95,21 +87,29 @@ export default function ClassBuckets({API, setSelected, openClassModal}){
         return `Class ${currentNode.label} has the following dependendents in other semesters: \n\t${dependents.map(e=>e.label).join(", ")}. Those classes have been removed from your plan.`
     }
 
-    const onDragEnd = ({ source, destination }) => {
-        let currentNode = nodes.filter(e=>e.bucket===source.droppableId)[source.index];
-        var bucket = buckets.filter(e=>e.id===destination.droppableId)[0];
-        var futureBuckets = buckets.filter(e=>e.index>bucket.index&&e.index!==0)
-        var previousBuckets = buckets.filter(e=>e.index<bucket.index&&e.index!==0)
-        var filteredArr = nodes.filter(e=>previousBuckets.some(f=>f.id===e.bucket))
-        var dependents = getDependents(currentNode, nodes.filter(e=>futureBuckets.some(f=>f.id===e.bucket)))
+    const getClassesWithMissingDependencies = (nodes, futureNodes) =>{
+        let retVal = [];
+        for(let elem of nodes){
+            if(!elem.children.every(e=>futureNodes.some(f=>f.id===e))){
+                retVal.push(elem)
+            }
+        }
+        console.log(retVal)
+        return retVal
+    }
 
+    const onDragEnd = ({ source, destination }) => {
         // dropped outside the list
         if (!destination) {
             return;
-        }else if(bucket.index!==0 && !isDropEnabled(currentNode, filteredArr)){
-            setModalMessage(getModalMissingMessage(currentNode, filteredArr))
-            return setModalOpen(true)
-        }else if(dependents.length>0 && source.droppableId !== destination.droppableId && source.droppableId!==buckets.filter(e=>e.label===PRIMARY_BUCKET).id){
+        }
+
+        let currentNode = nodes.filter(e=>e.bucket===source.droppableId)[source.index];
+        var bucket = buckets.filter(e=>e.id===destination.droppableId)[0];
+        var futureBuckets = buckets.filter(e=>e.index>bucket.index&&e.index!==0);
+        var dependents = getDependents(currentNode, nodes.filter(e=>futureBuckets.some(f=>f.id===e.bucket)));
+
+        if(dependents.length>0 && source.droppableId !== destination.droppableId && source.droppableId!==buckets.filter(e=>e.label===PRIMARY_BUCKET).id){
             setModalMessage(getModalRequirementMessage(currentNode, dependents))
             setModalOpen(true)
             removeDependents(dependents);
@@ -131,14 +131,10 @@ export default function ClassBuckets({API, setSelected, openClassModal}){
         }
     };
 
-    const isDropEnabled = (currentNode, filteredArr)=>{
-        return currentNode.children.length?currentNode.children.every(e=>filteredArr.some(f=>f.id===e)):true;
-    }
-
-    const onClassClick = (id)=>{
+    const onClassClick = React.useCallback((id)=>{
         setSelected(id)
         openClassModal()
-    }
+    }, [setSelected, openClassModal])
 
     React.useEffect(()=>{
         if(loading){
@@ -155,6 +151,56 @@ export default function ClassBuckets({API, setSelected, openClassModal}){
             })
         }
     }, [API,loading])
+
+    React.useEffect(()=>{
+        if(!buckets||!buckets.length){
+            return;
+        }
+        var futureBuckets = buckets.filter(e=>e.index!==0);
+        var primaryBucketItems = nodes.filter(e=>e.bucket===buckets.filter(f=>f.label===PRIMARY_BUCKET)[0].id)
+        setMissingClasses(getClassesWithMissingDependencies(primaryBucketItems, nodes.filter(e=>futureBuckets.some(f=>f.id===e.bucket))));
+    }, [nodes, buckets])
+
+    const bucketItems = React.useMemo(()=>{
+        return buckets.map(bucket=>{
+            return {
+                items: nodes.filter(node=>node.bucket===bucket.id).map(e=>{
+                    return {
+                        ...e, 
+                        children:e.children.map(c=>nodes.filter(f=>f.id===c)[0])
+                    }}
+                ),
+                bucket: bucket
+            }
+        })
+    }, [buckets, nodes])
+
+    const primaryBucket = React.useMemo(()=>{
+        if(bucketItems.length){
+            let {bucket, items} = bucketItems[0]
+            return <BucketSearchColumn bucket={bucket} 
+                        items={items}
+                        missing={missingClasses}
+                        populatedBuckets={nodes.filter(e=>buckets.filter(f=>f.id===e.bucket)[0].index>0)} 
+                        onClassClick={onClassClick}
+                    />
+        }
+    }, [bucketItems, missingClasses, onClassClick, nodes, buckets]);
+
+    const otherBuckets = React.useMemo(()=>{
+        return bucketItems.filter(e=>e.bucket.label!==PRIMARY_BUCKET).map(({bucket, items})=>
+            <>
+                <BucketItem bucket={bucket} 
+                            key={`bucket-item-${bucket.id}`}
+                            items={items}
+                            missing={missingClasses}
+                            onClassClick={onClassClick}
+                            collapseOpen={areAllOpen}
+                />
+                <hr/>
+            </>
+        )
+    }, [bucketItems, missingClasses, areAllOpen, onClassClick])
 
     if(loading){
         return <LoadingIndicator/>
@@ -177,22 +223,8 @@ export default function ClassBuckets({API, setSelected, openClassModal}){
                             padding:"5px"
                         }}>
                             <h3 style={{width:"100%", paddingLeft:"10px"}}>Required Courses</h3>
-                            {console.log(nodes.filter(node=>node.bucket===buckets[0].id).map(e=>{
-                                            return {
-                                                ...e, 
-                                                children:e.children.forEach(c=>nodes.filter(f=>f.id===c)[0]?console.log(nodes.filter(f=>f.id===c)[0]):console.log(c))
-                                            }}))}
-                            <BucketItem bucket={buckets[0]} 
-                                        items={nodes.filter(node=>node.bucket===buckets[0].id).map(e=>{
-                                            return {
-                                                ...e, 
-                                                children:e.children.map(c=>nodes.filter(f=>f.id===c)[0])
-                                            }})}
-                                        missing={missingClasses}
-                                        populatedBuckets={nodes.filter(e=>buckets.filter(f=>f.id===e.bucket)[0].index>0)} 
-                                        onClassClick={onClassClick}
-                                        isPrimary
-                            />
+                            <hr/>
+                            {primaryBucket}
                         </div>
                         <div style={{
                             flex:"1", 
@@ -202,19 +234,21 @@ export default function ClassBuckets({API, setSelected, openClassModal}){
                             alignItems: "center",
                             padding:"5px"
                         }}>
-                            <h3 style={{width:"100%", paddingLeft:"10px"}}>Your Plan</h3>
-                            {buckets.filter(e=>e.label!==PRIMARY_BUCKET).map((bucket)=>
-                                <BucketItem bucket={bucket} 
-                                            key={`bucket-item-${bucket.id}`}
-                                            items={nodes.filter(node=>node.bucket===bucket.id).map(e=>{
-                                                return {
-                                                    ...e, 
-                                                    children:e.children.map(c=>nodes.filter(f=>f.id===c)[0])
-                                                }})}
-                                            missing={missingClasses}
-                                            onClassClick={onClassClick}
-                                />
-                            )}
+                            <h3 style={{width:"100%", paddingLeft:"10px"}}>
+                                Your Plan
+                                <div style={{float:"right", margin:"auto 0"}}>
+                                    <a title={areAllOpen?"Close All":"Open All"}>
+                                        {
+                                            areAllOpen?
+                                            <BsList style={{width:"20px", height:"20px", margin:"2px"}} onClick={()=>setAllOpen(!areAllOpen)}/>:
+                                            <BsListNested style={{width:"20px", height:"20px", margin:"2px"}} onClick={()=>setAllOpen(!areAllOpen)} title="Open All"/>
+                                        }
+                                    </a>
+                                    <BsGearFill style={{width:"20px", height:"20px", margin:"2px"}}/>
+                                </div>
+                            </h3>
+                            <hr/>
+                            {otherBuckets}
                         </div>
                     
                 </div>
@@ -229,7 +263,31 @@ export default function ClassBuckets({API, setSelected, openClassModal}){
     )
 }
 
-function BucketItem({bucket,items, isDropDisabled, missing, onClassClick, populatedBuckets, collapseOpen}){
+function BucketSearchColumn({bucket,items, missing, onClassClick, populatedBuckets}){
+    const [search, setSearch] = React.useState("")
+
+    return (
+        <>
+            <div style={{paddingTop:"5px", width:"90%", margin:"0 auto"}}>
+                <Form.Control
+                    type="text"
+                    placeholder="Search"
+                    name="search"
+                    value={search}
+                    onChange={e=>setSearch(e.target.value)}
+                />
+            </div>
+            <BucketItem bucket={bucket} 
+                        items={items.filter(e=>!search || e.label.toLowerCase().indexOf(search.toLowerCase())>-1)}
+                        missing={missing}
+                        bucketMessage={search?"No classes match that search.":"No classes left to plan!"}
+                        populatedBuckets={populatedBuckets}
+                        onClassClick={onClassClick}/>
+        </>
+    )
+}
+
+function BucketItem({bucket,items, missing, onClassClick, populatedBuckets, collapseOpen, bucketMessage}){
     const [isCollapseOpen, setCollapseOpen] = React.useState(true);
 
     React.useEffect(()=>{
@@ -241,7 +299,6 @@ function BucketItem({bucket,items, isDropDisabled, missing, onClassClick, popula
     }, [items])
 
     const getStyle = (isDraggingOver) => ({
-        borderTop:"1px solid #003366",
         textAlign:'left',
         padding: `${grid} 0`,
         margin:`${grid} 0`,
@@ -250,25 +307,24 @@ function BucketItem({bucket,items, isDropDisabled, missing, onClassClick, popula
         flexBasis: "100%"
     });
     return (
-        <Droppable droppableId={bucket.id} key={bucket.id} isDropDisabled={isDropDisabled}>
+        <Droppable droppableId={bucket.id} key={bucket.id}>
             {(provided, snapshot)=>
                 <div
                     ref={provided.innerRef}
-                    style={getStyle(snapshot.isDraggingOver && !isDropDisabled)}
+                    style={getStyle(snapshot.isDraggingOver)}
                     {...provided.droppableProps}
                 >
-                    {console.log(items)}
                     <h5 style={{'marginBlockStart':'.3em', 'marginInlineStart':'.3em', marginBlockEnd:".3em", marginInlineEnd:".3em"}}>{
                         isCollapseOpen?
                         <BiCaretDown onClick={(e)=>setCollapseOpen(!isCollapseOpen)} style={{margin:"auto 0", padding:"0px 1%"}}/>:
                         <BiCaretRight onClick={()=>setCollapseOpen(!isCollapseOpen)} style={{margin:"auto 0", padding:"0px 1%"}}/>
                     }{bucket.label}</h5>
                     <Collapse isOpened={isCollapseOpen||snapshot.isDraggingOver}>
-                        <div style={{marginLeft:"25px"}}>
+                        <div style={{marginLeft:"25px", maxHeight:"80vh", overflowY:"auto", paddingRight:"3px"}}>
                         {
                             items.length||snapshot.isDraggingOver?
                             <ClassList items={items} missing={missing} onClassClick={onClassClick} populatedBuckets={populatedBuckets}/>:
-                            <p>{bucket.label===PRIMARY_BUCKET?"No classes left to plan!":"Add some classes to this semester!"}</p>
+                            <p>{bucket.label===PRIMARY_BUCKET?bucketMessage:"Add some classes to this semester!"}</p>
                         }
                         </div>
                     </Collapse>
@@ -301,6 +357,10 @@ function ClassItem({item, index, isMissing, onClassClick, populatedBuckets}){
         ...draggableStyle
     });
 
+    const isValid = React.useMemo(()=>{
+        return populatedBuckets?item.children.every(e=>e&&populatedBuckets.some(f=>f&&f.id===e.id)):false
+    }, [populatedBuckets, item])
+
     return (
         <Draggable key={item.id} draggableId={item.id} index={index} isDragDisabled={isMissing}>
             {(provided, snapshot) => (
@@ -312,9 +372,10 @@ function ClassItem({item, index, isMissing, onClassClick, populatedBuckets}){
                         snapshot.isDragging,
                         provided.draggableProps.style,
                         isMissing,
-                        populatedBuckets?item.children.every(e=>e&&populatedBuckets.some(f=>f&&f.id===e.id)):false
+                        isValid
                     )}
                     onClick={()=>onClassClick(item.id)}
+                    title={isMissing?"This class is missing requirements.":(isValid?"This class can be added to your plan.":null)}
                 >
                     <span>
                         {item.label}
